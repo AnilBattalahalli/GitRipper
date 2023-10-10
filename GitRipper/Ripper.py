80678194b201f112f52670e72a79a49d62111e53
sreenath_a <AnilSreenath@westat.com>
2023-10-10 17:11:47 -0400

    Added collectForks function and restructured the library

--- GitRipper/Ripper.py
+++ GitRipper/Ripper.py
@@ -22,209 +22,6 @@ def get_item(d, keys):
             return None
     return d
 
-def get_repository_info(owner, repo, token) -> dict:
-    """
-    Get the repository info from for a given owner and repo
-    Args:
-        owner (str)     : owner of the repository
-        repo (str)      : name of the repository
-        token (str)     : github token
-    Returns:
-        data_dict (dict): dictionary containing the repository info
-        rate_limit_info (dict): dictionary containing the rate limit info
-    """
-    url = 'https://api.github.com/graphql'
-    headers = {'Authorization': f'Bearer {token}'}
-
-    query = """
-    query {
-      repository(owner: "%s", name: "%s") {
-        name
-        description
-        shortDescriptionHTML
-        url
-        createdAt
-        updatedAt
-        pushedAt
-        forkCount
-        stargazerCount: stargazers {
-          totalCount
-        }
-        issues(states: OPEN) {
-          totalCount
-        }
-        pullRequests(states: OPEN) {
-          totalCount
-        }
-        owner {
-          login
-        }
-        licenseInfo {
-            name
-            spdxId
-            url
-            }
-        object(expression: "HEAD:README.md") {
-          ... on Blob {
-            text
-          }
-        }
-      }
-      viewer {
-            login
-            }
-      rateLimit {
-        limit
-        cost
-        remaining
-        resetAt
-      }
-    }
-    """ % (owner, repo)
-
-    response = requests.post(url, json={'query': query}, headers=headers)
-    if response.status_code != 200:
-        print(response.text)
-        data_dict = {'owner':owner, 'repo':repo,'name': None, 'description': None, 'owner': None, 'licenseName': None,
-                'licenseSpdxId': None, 'licenseUrl': None, 'shortDescriptionHTML': None,
-                'repourl': None, 'createdAt': None, 'updatedAt': None, 'pushedAt': None,
-                'forkCount': None, 'stargazerCount': None, 'issuesCount': None,
-                'pullRequestsCount': None, 'readme': None}
-        return None, None
-    result = response.json()
-    name = get_item(result, ['data','repository','name'])
-    description = get_item(result,['data','repository','description'])
-    shortDescriptionHTML = get_item(result,['data','repository','shortDescriptionHTML'])
-    repourl = get_item(result,['data','repository','url'])
-    createdAt = get_item(result,['data','repository','createdAt'])
-    updatedAt = get_item(result,['data','repository','updatedAt'])
-    pushedAt = get_item(result,['data','repository','pushedAt'])
-    forkCount = get_item(result,['data','repository','forkCount'])
-    stargazerCount = get_item(result,['data','repository','stargazerCount','totalCount'])
-    issuesCount = get_item(result,['data','repository','issues','totalCount'])
-    pullRequestsCount = get_item(result,['data','repository','pullRequests','totalCount'])
-    readme = get_item(result,['data','repository','object','text'])
-    owner = get_item(result,['data','repository','owner','login'])
-    licenseName = get_item(result,['data','repository','licenseInfo','name'])
-    licenseSpdxId = get_item(result,['data','repository','licenseInfo','spdxId'])
-    licenseUrl = get_item(result,['data','repository','licenseInfo','url'])
-
-    # get all rate limit info
-    login = get_item(result,['data','viewer','login'])
-    limit = get_item(result,['data','rateLimit','limit'])
-    cost = get_item(result,['data','rateLimit','cost'])
-    remaining = get_item(result,['data','rateLimit','remaining'])
-    resetAt = get_item(result,['data','rateLimit','resetAt'])
-    rate_limit_info = {'login': login, 'limit': limit, 'cost': cost,
-                       'remaining': remaining, 'resetAt': resetAt}
-
-    data_dict = {'owner':owner, 'repo':repo,'name': name, 'description': description, 'owner': owner, 'licenseName': licenseName, 
-                'licenseSpdxId': licenseSpdxId, 'licenseUrl': licenseUrl, 'shortDescriptionHTML': shortDescriptionHTML,
-                'repourl': repourl, 'createdAt': createdAt, 'updatedAt': updatedAt, 'pushedAt': pushedAt,
-                'forkCount': forkCount, 'stargazerCount': stargazerCount, 'issuesCount': issuesCount,
-                'pullRequestsCount': pullRequestsCount, 'readme': readme}
-
-    return data_dict, rate_limit_info
-
-
-def get_all_commits(owner, repo, token, since) -> pd.DataFrame:
-    """
-    Get all commits information from a repository since a given date
-    Args:
-        owner (str)     : owner of the repository
-        repo (str)      : name of the repository
-        token (str)     : github token
-        since (str)     : date in ISO 8601 format
-    Returns:
-        df_out (pd.DataFrame): dataframe containing the commits information
-    """
-    url = 'https://api.github.com/graphql'
-    headers = {'Authorization': f'Bearer {token}'}
-    query = """
-    query($cursor: String) {
-        repository(owner: "%s", name: "%s") {
-            defaultBranchRef {
-                target {
-                    ... on Commit {
-                    history(first: 100, after: $cursor,  since: "%s") {
-                        pageInfo {
-                        hasNextPage
-                        endCursor
-                        }
-                        edges {
-                        node {
-                            oid
-                            messageHeadline
-                            author {
-                            name
-                            email
-                            date
-                            user {
-                                login
-                                location
-                                company
-                                pronouns
-                                bio
-                                websiteUrl
-                                twitterUsername
-                            }
-                            }
-                            additions
-                            deletions
-                        }
-                        }
-                    }
-                    }
-                }
-            }
-        }
-    }
-    """ % (owner, repo, since)
-    # print(query)
-    df_rows = []
-    has_next_page = True
-    cursor = None
-    while has_next_page:
-        variables = {'cursor': cursor}
-        response = requests.post(
-            url, json={'query': query, 'variables': variables}, headers=headers)
-        if response.status_code != 200:
-            print(response.text)
-            return None
-        result = response.json()
-        history = get_item(result,['data','repository','defaultBranchRef','target','history'])
-        hists = get_item(history, ['edges'])
-        if hists is None:
-            hists = []
-        for edge in hists:
-            node = get_item(edge,['node'])
-            username = get_item(node,['author','user','login'])
-            location = get_item(node,['author','user','location'])
-            company = get_item(node,['author','user','company'])
-            pronouns = get_item(node,['author','user','pronouns'])
-            bio = get_item(node,['author','user','bio'])
-            websiteUrl = get_item(node,['author','user','websiteUrl'])
-            twitterUsername = get_item(node,['author','user','twitterUsername'])
-            df_rows.append([get_item(node,['oid']), get_item(node,['messageHeadline']), 
-                            get_item(node,['author','name']), 
-                            get_item(node,['author','email']), username, location,
-                           company, pronouns, bio, websiteUrl, twitterUsername, 
-                           get_item(node, ['author','date']), get_item(node,['additions']), 
-                           get_item(node, ['deletions'])])
-        has_next_page = get_item(history,['pageInfo','hasNextPage'])
-        cursor = get_item(history,['pageInfo','endCursor'])
-    df_out = pd.DataFrame(df_rows, columns=['oid', 'messageHeadline', 'author_name', 'author_email', 'author_user_login', 'author_user_location', 'author_user_company',
-                          'author_user_pronouns', 'author_user_bio', 'author_user_websiteUrl', 'author_user_twitterUsername', 'author_date', 'additions', 'deletions'])
-    # add repo name and owner to the dataframe
-    df_out['repo_name'] = repo
-    df_out['repo_owner'] = owner
-    #Put owner and repo name in the first two columns
-    cols = df_out.columns.tolist()
-    cols = cols[-2:] + cols[:-2]
-    df_out = df_out[cols]
-    return df_out
-
-
 def githubKeysInfo(token) -> dict:
     """
     Get the rate limit info from github api for a token
@@ -351,9 +148,95 @@ class collect:
         """
         if token is None:
             token = self.getBestKey()
-        df = get_all_commits(owner, repo, token, since)
-        self.refreshKeysHealth()
-        return df
+        url = 'https://api.github.com/graphql'
+        query = """
+        query($cursor: String) {
+            repository(owner: "%s", name: "%s") {
+                defaultBranchRef {
+                    target {
+                        ... on Commit {
+                        history(first: 100, after: $cursor,  since: "%s") {
+                            pageInfo {
+                            hasNextPage
+                            endCursor
+                            }
+                            edges {
+                            node {
+                                oid
+                                messageHeadline
+                                author {
+                                name
+                                email
+                                date
+                                user {
+                                    login
+                                    location
+                                    company
+                                    pronouns
+                                    bio
+                                    websiteUrl
+                                    twitterUsername
+                                }
+                                }
+                                additions
+                                deletions
+                            }
+                            }
+                        }
+                        }
+                    }
+                }
+            }
+        }
+        """ % (owner, repo, since)
+        # print(query)
+        df_rows = []
+        has_next_page = True
+        cursor = None
+        num = 0
+        while has_next_page:
+            headers = {'Authorization': f'Bearer {token}'}
+            variables = {'cursor': cursor}
+            response = requests.post(
+                url, json={'query': query, 'variables': variables}, headers=headers)
+            if response.status_code != 200:
+                print(response.text)
+                return None
+            result = response.json()
+            history = get_item(result,['data','repository','defaultBranchRef','target','history'])
+            hists = get_item(history, ['edges'])
+            if hists is None:
+                hists = []
+            for edge in hists:
+                node = get_item(edge,['node'])
+                username = get_item(node,['author','user','login'])
+                location = get_item(node,['author','user','location'])
+                company = get_item(node,['author','user','company'])
+                pronouns = get_item(node,['author','user','pronouns'])
+                bio = get_item(node,['author','user','bio'])
+                websiteUrl = get_item(node,['author','user','websiteUrl'])
+                twitterUsername = get_item(node,['author','user','twitterUsername'])
+                df_rows.append([get_item(node,['oid']), get_item(node,['messageHeadline']), 
+                                get_item(node,['author','name']), 
+                                get_item(node,['author','email']), username, location,
+                            company, pronouns, bio, websiteUrl, twitterUsername, 
+                            get_item(node, ['author','date']), get_item(node,['additions']), 
+                            get_item(node, ['deletions'])])
+            has_next_page = get_item(history,['pageInfo','hasNextPage'])
+            cursor = get_item(history,['pageInfo','endCursor'])
+            num += 1
+            if num % 10 == 0:
+                token = self.getBestKey()
+        df_out = pd.DataFrame(df_rows, columns=['oid', 'messageHeadline', 'author_name', 'author_email', 'author_user_login', 'author_user_location', 'author_user_company',
+                            'author_user_pronouns', 'author_user_bio', 'author_user_websiteUrl', 'author_user_twitterUsername', 'author_date', 'additions', 'deletions'])
+        # add repo name and owner to the dataframe
+        df_out['repo_name'] = repo
+        df_out['repo_owner'] = owner
+        #Put owner and repo name in the first two columns
+        cols = df_out.columns.tolist()
+        cols = cols[-2:] + cols[:-2]
+        df_out = df_out[cols]
+        return df_out
 
     def getRepoInfo(self, owner, repo, token=None) -> dict:
         """
@@ -367,6 +250,155 @@ class collect:
         """
         if token is None:
             token = self.getBestKey()
-        d, rl_dict = get_repository_info(owner, repo, token)
-        self.keys_dict[token] = rl_dict
-        return d
+        url = 'https://api.github.com/graphql'
+        headers = {'Authorization': f'Bearer {token}'}
+
+        query = """
+        query {
+        repository(owner: "%s", name: "%s") {
+            name
+            description
+            shortDescriptionHTML
+            url
+            createdAt
+            updatedAt
+            pushedAt
+            forkCount
+            stargazerCount: stargazers {
+            totalCount
+            }
+            issues(states: OPEN) {
+            totalCount
+            }
+            pullRequests(states: OPEN) {
+            totalCount
+            }
+            owner {
+            login
+            }
+            licenseInfo {
+                name
+                spdxId
+                url
+                }
+            object(expression: "HEAD:README.md") {
+            ... on Blob {
+                text
+            }
+            }
+        }
+        viewer {
+                login
+                }
+        rateLimit {
+            limit
+            cost
+            remaining
+            resetAt
+        }
+        }
+        """ % (owner, repo)
+
+        response = requests.post(url, json={'query': query}, headers=headers)
+        if response.status_code != 200:
+            print(response.text)
+            data_dict = {'owner':owner, 'repo':repo,'name': None, 'description': None, 'owner': None, 'licenseName': None,
+                    'licenseSpdxId': None, 'licenseUrl': None, 'shortDescriptionHTML': None,
+                    'repourl': None, 'createdAt': None, 'updatedAt': None, 'pushedAt': None,
+                    'forkCount': None, 'stargazerCount': None, 'issuesCount': None,
+                    'pullRequestsCount': None, 'readme': None}
+            return None, None
+        result = response.json()
+        name = get_item(result, ['data','repository','name'])
+        description = get_item(result,['data','repository','description'])
+        shortDescriptionHTML = get_item(result,['data','repository','shortDescriptionHTML'])
+        repourl = get_item(result,['data','repository','url'])
+        createdAt = get_item(result,['data','repository','createdAt'])
+        updatedAt = get_item(result,['data','repository','updatedAt'])
+        pushedAt = get_item(result,['data','repository','pushedAt'])
+        forkCount = get_item(result,['data','repository','forkCount'])
+        stargazerCount = get_item(result,['data','repository','stargazerCount','totalCount'])
+        issuesCount = get_item(result,['data','repository','issues','totalCount'])
+        pullRequestsCount = get_item(result,['data','repository','pullRequests','totalCount'])
+        readme = get_item(result,['data','repository','object','text'])
+        owner = get_item(result,['data','repository','owner','login'])
+        licenseName = get_item(result,['data','repository','licenseInfo','name'])
+        licenseSpdxId = get_item(result,['data','repository','licenseInfo','spdxId'])
+        licenseUrl = get_item(result,['data','repository','licenseInfo','url'])
+
+        # get all rate limit info
+        login = get_item(result,['data','viewer','login'])
+        limit = get_item(result,['data','rateLimit','limit'])
+        cost = get_item(result,['data','rateLimit','cost'])
+        remaining = get_item(result,['data','rateLimit','remaining'])
+        resetAt = get_item(result,['data','rateLimit','resetAt'])
+        rate_limit_info = {'login': login, 'limit': limit, 'cost': cost,
+                        'remaining': remaining, 'resetAt': resetAt}
+
+        data_dict = {'owner':owner, 'repo':repo,'name': name, 'description': description, 'owner': owner, 'licenseName': licenseName, 
+                    'licenseSpdxId': licenseSpdxId, 'licenseUrl': licenseUrl, 'shortDescriptionHTML': shortDescriptionHTML,
+                    'repourl': repourl, 'createdAt': createdAt, 'updatedAt': updatedAt, 'pushedAt': pushedAt,
+                    'forkCount': forkCount, 'stargazerCount': stargazerCount, 'issuesCount': issuesCount,
+                    'pullRequestsCount': pullRequestsCount, 'readme': readme}
+        self.keys_dict[token] = rate_limit_info
+        return data_dict
+    
+    def collectForks(self, owner, repo, token=None) -> pd.DataFrame:
+        if token is None:
+            token = self.getBestKey()
+        endpoint = 'https://api.github.com/graphql'
+        query = '''
+            query($owner: String!, $repo: String!, $cursor: String) {
+                repository(owner: $owner, name: $repo) {
+                forks(first: 100, after: $cursor) {
+                    pageInfo {
+                    endCursor
+                    hasNextPage
+                    }
+                    nodes {
+                    owner {
+                        login
+                    }
+                    name
+                    }
+                }
+                }
+            }
+        '''
+        variables = {
+            'owner': owner,
+            'repo': repo,
+            'cursor': None  # Set to None for the initial request
+        }
+        columns = ['owner', 'repo', 'fork_owner', 'fork_repo']
+        data = []
+        num = 0
+        while True:
+            headers = {'Authorization': 'Bearer ' + token}
+            response = requests.post(
+                endpoint,
+                headers=headers,
+                json={'query': query, 'variables': variables}
+            ).json()
+
+            # Extract information from the response
+            forks = response['data']['repository']['forks']['nodes']
+            for fork in forks:
+                data.append([
+                    owner,
+                    repo,
+                    fork['owner']['login'],
+                    fork['name']
+                ])
+
+            # Check if there are more pages
+            page_info = response['data']['repository']['forks']['pageInfo']
+            if page_info['hasNextPage']:
+                variables['cursor'] = page_info['endCursor']
+            else:
+                break
+            num += 1
+            if num % 10 == 0:
+                token = self.getBestKey()
+        df = pd.DataFrame(data, columns=columns)
+        return df
